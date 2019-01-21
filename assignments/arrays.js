97c9c4a4983fb75f7fa2e33d7bfaf728a8c3245d
Giacomo Benati <giacomobenati@mailbox.org>
2019-01-21 15:56:36 +0100

    (STRETCH) reshaped arrays.js to use forEach loops and arrow syntax

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -89,29 +89,40 @@ console.log(carModels);
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-for (i = 0; i < inventory.length; i++) {
-    carYears.push(inventory[i].car_year)    
-}
+// for (i = 0; i < inventory.length; i++) {
+//     carYears.push(inventory[i].car_year)    
+// }
+inventory.forEach(item => carYears.push(item.car_year))
 console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
-for (let i = 0; i < carYears.length; i++) {
-    if (carYears[i] < 2000) {
-        oldCars.push(carYears[i]);
-    }
-}
+// for (let i = 0; i < carYears.length; i++) {
+//     if (carYears[i] < 2000) {
+//         oldCars.push(carYears[i]);
+//     }
+// }
+carYears.forEach(item => {
+    if (item < 2000) {
+    oldCars.push(item);
+} 
+})
 console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi =[];
-for (let i = 0; i < inventory.length; i++) {
-    if (inventory[i].car_make === "Audi" || inventory[i].car_make === "BMW") {
-        BMWAndAudi.push(inventory[i]);
+// for (let i = 0; i < inventory.length; i++) {
+//     if (inventory[i].car_make === "Audi" || inventory[i].car_make === "BMW") {
+//         BMWAndAudi.push(inventory[i]);
+//     }
+// }
+inventory.forEach(item => {
+    if (item.car_make === "Audi" || item.car_make === "BMW") {
+        BMWAndAudi.push(item);
     }
-}
+})
 //JSON.stringify(BMWAndAudi, ["id", "brand", "model", "year"]);
 console.log(JSON.stringify(BMWAndAudi, ["id", "car_make", "car_model", "car_year"], "/t"));
 
